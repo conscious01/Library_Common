a7bfe8fe51af1d198286f161edc2323bd5cdcd50
Robb <Mr.W@myemail.com>
2022-04-24 16:31:07 +0800

    基本完工

--- app/src/main/java/com/common/myapplication/ui/activity/MainActivity.java
+++ app/src/main/java/com/common/myapplication/ui/activity/MainActivity.java
@@ -15,13 +15,12 @@ import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.listener.OnItemClickListener;
 import com.common.lib_base.base_view.BaseActivity;
 import com.common.lib_base.common.utils.ViewUtils;
+import com.common.lib_base.module.EntityBaseDialog;
 import com.common.myapplication.adapter.AdapterDemo;
 import com.common.myapplication.MyApp;
 import com.common.myapplication.ui.dialog.VBDialog;
 import com.example.myapplication.R;
-import com.hww.skcap.HC_SKCAP;
-import com.hww.skcap.info.HealthCodeType;
-import com.hww.skcap.info.JKMResultStatus;
+
 import com.lxj.xpopup.XPopup;
 import java.util.ArrayList;
 import java.util.List;
@@ -62,52 +61,7 @@ public class MainActivity extends BaseActivity implements OnItemClickListener {
 
     }
 
-    private void testMethod() {
 
-        String snCode = "C3Fak4rsEy+d3gRyIBWlMI2BNarOFEgUIfpetXl7JFekZDtKt2Fcy4MCmg2EfHorsWGDi0h4tSwj2hdyBhaB3eLfePgncM4lwrzagwXvQ0xulnTjXHueqXv7QrbfuBPk/M3FfCc8mTa+VmfMs5VweDzvC4VmMuedr9bB8thCAHibrVl6RGZZYnv839WTNubaEUbuh9UAC7g/PtALPgD949M0GuaU4LbNIpuSgU/QhFpxq8LVFdpIGW3AMfYTO4YchB+ndeFEsILctyuNnipc1pTBaOQi8ATbaPKXV5bK1BCPjrCRWNPGH3Ya1dXhza3bkyZH5aaBHotVAuhHCZ80VA==";
-
-        String tag = "gkm1";
-        String pathDir = MyApp.mInstantce.getExternalFilesDir(null).getPath();
-
-        HC_SKCAP.getInstance().init(this, "szszzxkj", "5F6E0C5E6571E2C6", tag, pathDir, true, "");
-
-        // 正式调用应使用下面的初始化方式
-        try {
-            HC_SKCAP.getInstance()
-                    .init(this, "szszzxkj", "5F6E0C5E6571E2C6", tag, pathDir, true, "");
-
-//            Skcap.init(0, 0, 0, pathDir, true,
-//                    "szszzxkj",//与我方技术人员联系获取
-//                    "5F6E0C5E6571E2C6",//与我方技术人员联系获取
-//                    tag);
-//
-//            // 生成设备描述JSON字符串
-//            JSONObject jsonObject = new JSONObject()
-//                    .put("model", "jerry")
-//                    .put("vendor", "ibm")
-//                    .put("manufacturer", "BYD")
-//                    .put("location", "XX省XX市XX区XX街道");
-//            // 定义设备描述信息
-//            Skcap.defineDeviceInfo(jsonObject.toString());
-
-        } catch (Exception e) {
-            LogUtils.e(e);
-            e.printStackTrace();
-        } catch (Throwable e) {
-            e.printStackTrace();
-            LogUtils.e(e);
-
-        }
-
-
-    }
-
-
-    private void testMehtod02() {
-        JKMResultStatus jkmResultStatus = HC_SKCAP.getInstance()
-                .queryFromIdCard(HealthCodeType.HC_SKM_JS, "130428199002260032", "张世鹏", "");
-        LogUtils.i(jkmResultStatus);
-    }
 
 
     @Override
@@ -115,13 +69,10 @@ public class MainActivity extends BaseActivity implements OnItemClickListener {
             int position) {
         switch (position) {
             case 0: //图片展示
-//                startActivity(ImageActivity.class);
-                testMethod();
+                startActivity(ImageActivity.class);
 
                 break;
             case 1://弹窗
-
-                testMehtod02();
 
 //                EntityBaseDialog entityBaseDialog = new EntityBaseDialog();
 //                entityBaseDialog.setTitle("主app");
